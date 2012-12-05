121dd404eed6b6907c1165eeacaa3969222d45bc
Julian "Andrakis" Thatcher <julian@noblesamurai.com>
2012-12-05 23:31:56 +1100

    vis: implementation working, remove world.erl
    The vis implementation is working, but not custom behaviour is
    presently provided.

--- src/vis.erl
+++ src/vis.erl
@@ -14,117 +14,186 @@
 
 -module(vis).
 
+-export([new/2, visualize/1]).
+
 -include("locations.hrl").
 
--record(map_entry, {
-	pos_2d :: pos_2d(),
-	content :: string()
-}).
--type map_entry() :: #map_entry{}.
+-include("test.hrl").
 
--record(map_done, {
-	pos_2d :: pos_2d(),
-	location :: location()
-}).
--type map_done() :: #map_done{}.
+-type loc() :: location_id() | location().
+-export_type([loc/0]).
 
 -record(vis, {
-	lower_x = 0         :: integer(),
-	lower_y = 0         :: integer(),
-	upper_x = 0         :: integer(),
-	upper_y = 0         :: integer(),
-	map = []            :: [map_entry()],
-	done = []           :: [map_done()],
-	% How far should we travel for mapping? Aborts after this length.
-	search_width = all  :: integer() | all,
-	search_height = all :: integer() | all,
-	module = ?MODULE    :: atom()
+	locations = [] :: {location_id(), loc()},
+	root           :: loc(),
+	min_x = -1     :: neg_integer(),
+	min_y = -1     :: neg_integer(),
+	max_x =  1     :: pos_integer(),
+	max_y =  1     :: pos_integer(),
+	visual = []    :: [{pos(), binary()}],
+	done = []      :: [{pos(), done}]
 }).
+-type vis() :: #vis{}.
 
--export([handle_render/1, handle_border/2]).
+%%============================================================================
+%% API
+%%============================================================================
+%% @doc Create a new visualization.
+-spec new(Location::loc(), Locations::[loc()]) -> vis().
+new(Location, Locations) ->
+	#vis{
+		locations = Locations,
+		root = Location
+	}.
 
+%% @doc Visualize and render the given vis.
+%% @private
+-spec visualize(Vis::vis()) -> [[binary()]].
+visualize(#vis{} = Vis0) ->
+	{Vis1, Actions} = map_location(Vis0#vis.root, {0, 0}, Vis0),
+	Vis2 = map_loop(Vis1, Actions),
+	XSeq = lists:seq(Vis2#vis.min_x, Vis2#vis.max_x),
+	YSeq = lists:seq(Vis2#vis.min_y, Vis2#vis.max_y),
+	[ begin
+		[ case lists:keyfind({X, Y}, 1, Vis2#vis.visual) of
+			false -> <<".">>;
+			{_, Tile} -> Tile
+		  end || X <- XSeq ] ++ [<<"\n">>]
+	  end || Y <- YSeq ].
 
 %%============================================================================
 %% Reference behaviour implementation
 %%============================================================================
 
-%% @doc Given a record of some sort, render it to text.
--spec handle_render(term()) -> string().
-handle_render(#location{}) -> " ";
-handle_render(#actor{}) -> "@";
-handle_render(_) -> "?".
+%%============================================================================
+%% Internal functions
+%%============================================================================
 
-%% @doc Given a direction and location record, render the border.
--spec handle_border(direction_2d(), location()) -> string().
-handle_border(_, #location{ neighbours = [] }) ->
-	"*";
-handle_border(Direction, #location{ neighbours = Neighbours }) ->
-	case proplists:get_value(Direction, Neighbours) of
-		undefined -> "*";
-		#location{} = Loc -> handle_render(Loc)
-	end.
+%% @doc Recusrively perform the map_loop calls required.
+-spec map_loop(Vis::vis(), Actions::[{Location::loc(), Position::pos(), Prev::location_id()}]) -> [[binary()]].
+map_loop(Vis0, [{Location, Position, Prev} | Tail]) ->
+	{Vis1, Actions} = map_location(Location, Position, Vis0, Prev),
+	map_loop(Vis1, Actions ++ Tail);
+map_loop(Vis0, _) ->
+	Vis0.
 
-%% OTHER
--spec map(InitialLocation::location()) -> #vis{}.
-map(InitialLocation) ->
-	map(InitialLocation, {0, 0}, #vis{}).
+%% @see map_location/4
+%% @private
+-spec map_location(Location::loc(), Pos::pos(), Vis::vis()) -> [[binary()]].
+map_location(Location, Pos, #vis{} = Vis) ->
+	map_location(Location, Pos, Vis, nil).
 
--spec map(Location::location(), pos_2d(), Vis::#vis{}) -> #vis{}.
-map(#location{} = Loc, #pos_2d{ x = X, y = Y } = Pos2d, #vis{ map = Map, done = Done } = Vis0) ->
-	% Prevent recursion
-	case lists:keyfind(Pos2d, #map_done.pos_2d, Done) of
-		false ->
-			Module = Vis0#vis.module,
-			Entry = #map_entry{
-				pos_2d = Pos2d,
-				content = Module:handle_render(Loc)
-			},
-			Vis1 = recalculate_boundries(Pos2d, Vis0),
-			Vis2 = Vis1#vis{
-				map = [Entry | Vis1#vis.map],
-				done = [{Pos2d, Loc} | Done]
-			},
-			% Render the boundries in each direction
-			Map0 = Vis2#vis.map,
-			Map1 = lists:foldl(fun(Direction, MapAcc) ->
-				Pos = world_relative_pos2d(Direction, Pos2d),
-				case proplists:get_value(Pos, MapAcc) of
-					undefined ->
-						[Module:handle_border(Direction, Loc) | MapAcc];
-					_ ->
-						MapAcc
-				end
-			end, Map0, ?DIRECTIONS_2D),
-			Vis2#vis{
-				map = Map1
-			};
-		_ ->
-			Vis0
-	end.
+%% @doc Map a given location, and all of its connected locations.
+%% @private
+-spec map_location(Location::loc(), Pos::pos(), Vis::vis(), Prev::location_id()) -> [[binary()]].
+map_location(Location, Pos, #vis{} = Vis, Prev) ->
+	IsDone = lists:keyfind(Pos, 1, Vis#vis.done) == {Pos, done},
+	maybe_map_location(IsDone, Location, Pos, Vis, Prev).
 
-recalculate_boundries(#pos_2d{ x = X, y = Y }, #vis{} = Vis0) ->
-	Vis0#vis{
-		lower_x = min(Vis0#vis.lower_x, X - 1),
-		lower_y = min(Vis0#vis.lower_y, Y - 1),
-		upper_x = max(Vis0#vis.upper_x, X + 1),
-		upper_y = max(Vis0#vis.upper_y, Y + 1)
-	}.
+-define(if_true(Test, New, Old), if Test -> New; true -> Old end).
 
--spec world_relative_pos2d(Direction::direction_2d(), pos_2d()) ->
-	pos_2d().
-world_relative_pos2d(Direction, #pos_2d{ x = X, y = Y } = Pos2d) ->
-	case Direction of
-		north -> Pos2d#pos_2d{ y = Y - 1 };
-		south -> Pos2d#pos_2d{ y = Y + 1 };
-		east  -> Pos2d#pos_2d{ x = X + 1 };
-		west  -> Pos2d#pos_2d{ x = X - 1 }
-	end.
+%% @doc Map a location only if it has not been done yet, as specified by IsDone.
+%% @private
+-spec maybe_map_location(IsDone::boolean(), Location::loc(), Pos::pos(), Vis::vis(),
+		Prev::location_id()) -> [[binary()]].
+maybe_map_location(true,  _,    _,            Vis, _) ->
+	{Vis, []};
+maybe_map_location(false, Location, {X, Y} = Pos, #vis{ visual = Visual0 } = Vis0, Prev0) ->
+	Prev1 = if
+		Prev0 == nil -> ?location_id(Location);
+		true -> Prev0
+	end,
+	Tile = loc:tile(Location),
+	Visual1 = [{Pos, Tile} | Visual0],
+	Visual2 = lists:foldl(fun(Direction, VisualAcc) ->
+		Position = loc:position(Direction, Pos),
+		case lists:keyfind(Position, 1, VisualAcc) of
+			false ->
+				case loc:neighbour(Direction, Location) of
+					#neighbour{ id = Id } ->
+						Neighbour = lists:keyfind(Id, 1, Vis0#vis.locations),
+						[{Position, loc:border(Direction, Neighbour)} | VisualAcc];
+					_ -> VisualAcc
+				end;
+			_ ->
+				VisualAcc
+		end
+	end, Visual1, ?directions),
+	Vis1 = Vis0#vis{
+		done = [{Pos, done} | Vis0#vis.done],
+		min_x = ?if_true(X - 1 < Vis0#vis.min_x, X - 1, Vis0#vis.min_x),
+		min_y = ?if_true(Y - 1 < Vis0#vis.min_y, Y - 1, Vis0#vis.min_y),
+		max_x = ?if_true(X + 1 > Vis0#vis.max_x, X + 1, Vis0#vis.max_x),
+		max_y = ?if_true(Y + 1 > Vis0#vis.max_y, Y + 1, Vis0#vis.max_y),
+		visual = Visual2
+	},
+	Actions = lists:foldl(fun(Direction, ActionsAcc) ->
+		case loc:neighbour(Direction, Location) of
+			#neighbour{ id = Id } when Id /= Prev1 ->
+				{_, Neighbour} = lists:keyfind(Id, 1, Vis1#vis.locations),
+				Position = loc:position(Direction, Pos),
+				[{Neighbour, Position, Prev1} | ActionsAcc];
+			_ ->
+				ActionsAcc
+		end
+	end, [], ?directions),
+	{Vis1, Actions}.
 
--spec world_relative_pos3d(Direction::direction_3d(), pos_3d()) ->
-	pos_3d().
-world_relative_pos3d(up, #pos_3d{ z = Z } = Pos3d) ->
-	Pos3d#pos_3d{ z = Z + 1 };
-world_relative_pos3d(down, #pos_3d{ z = Z } = Pos3d) ->
-	Pos3d#pos_3d{ z = Z - 1 };
-world_relative_pos3d(Direction, #pos_3d{ xy = XY } = Pos3d) ->
-	Pos3d#pos_3d{ xy = world_relative_pos2d(Direction, XY) }.
+%%============================================================================
+%% Test functions
+%%============================================================================
+
+-ifdef(TEST).
+
+% Required to use Mockgyver.
+vis_test_() ->
+	?WITH_MOCKED_SETUP(fun setup/0, fun cleanup/1).
+
+setup() ->
+	ok.
+
+cleanup(_) ->
+	ok.
+
+%% TODO: Make this test more low-level.
+visualize_test(_) ->
+
+	% Simulate locations as best we can
+	L1 = list_to_pid("<0.1.0>"),
+	L2 = list_to_pid("<0.2.0>"),
+	L3 = list_to_pid("<0.3.0>"),
+	Ls = [ {L, L} || L <- [L1, L2, L3] ],
+
+	?WHEN(loc:neighbour(Dir, L) -> case Dir of
+		% East points L1 to L2
+		east when L == L1 -> #neighbour{ id = L2 };
+		% West points L2 to L1
+		west when L == L2 -> #neighbour{ id = L1 };
+		% South points L2 to L3
+		south when L == L2 -> #neighbour{ id = L3 };
+		% North points L3 to L2
+		north when L == L3 -> #neighbour{ id = L2 };
+		_ -> false
+	end),
+	?WHEN(loc:tile(L) -> case L of
+		L1 -> <<"!">>;
+		L2 -> <<"2">>;
+		L3 -> <<"3">>;
+		_ -> <<" ">>
+	end),
+	?WHEN(loc:border(_, _) -> <<".">>),
+
+	Vis = new(L1, Ls),
+	Map = visualize(Vis),
+	?assertEqual([
+		[<<".">>,<<".">>,<<".">>,<<".">>,<<"\n">>],
+		[<<".">>,<<"!">>,<<"2">>,<<".">>,<<"\n">>],
+		[<<".">>,<<".">>,<<"3">>,<<".">>,<<"\n">>],
+		[<<".">>,<<".">>,<<".">>,<<".">>,<<"\n">>]],
+		Map),
+	
+	?debugFmt("Pretty: ~s~n", [Map]),
+	
+	ok.
+
+-endif.
